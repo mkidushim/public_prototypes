9e5030e649eeb338be35501e93cee3e096ab3535
mkidushim <mkidushim@gmail.com>
2015-05-25 13:08:44 -0700

    ajax prototype

--- js_ajax/main.js
+++ js_ajax/main.js
@@ -10,7 +10,8 @@ $(document).ready(function(){
 				console.log('result2: ', window.result)
 				for (var i = 0; i < window.result.feed.entry.length-1; i++){
 					var third_img_movie = window.result.feed.entry[i]['im:image'][2].label;
-					var m_name = window.result.feed.entry[i].title.label;
+					var m_name = window.result.feed.entry[i]['im:name'].label;
+					var m_artist = window.result.feed.entry[i]['im:artist'].label;
 					console.log('third_img_movie:', third_img_movie)
 					var container = $('<figure>',{
 						class: 'col-md-1'
@@ -19,12 +20,13 @@ $(document).ready(function(){
 					src: third_img_movie
 
 					})
-					var title = $('<figcaption>',{
-					text: m_name
-					})
-
-					$(container).append(image).append(title);
+					var m_info = $('<div class= "info">')
+					var title = $('<div class= "title">' + m_name +'</div>')
+					var artist = $('<div class= "artist">'+ m_artist +'</div>')
 					$('#main').append(container)
+					$(container).append(image,m_info)
+					$(m_info).append(title,artist)
+					
 
 				}
 			}
