ed186b5758cb5f6fd605e38c3a31d72a42663490
mkidushim <mkidushim@gmail.com>
2015-06-03 11:38:37 -0700

    Finished php_includes

---
+++ php_includes/index_main.php
@@ -0,0 +1,5 @@
+<?php
+include("main.php");
+echo "<div>I39m not in the main content or the footer</div>";
+include("footer.php");
+?>
---
+++ php_includes/main.php
@@ -0,0 +1,8 @@
+<main id="main_content">
+    Main Content
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel mollis diam. Mauris at vulputate erat. 
+    Vestibulumante orci, molestie sit amet cursus vitae, elementum sed felis. Nullam a lectus neque. Maecenas f
+    eugiat convallis metuvitae egestas. Morbi pretium, velit vel porta sagittis, augue urna consequat augue, vitae 
+    tempor quam nisi a ipsum. Pellentesque tristique aliquet rutrum.
+<img style="height: 100px; width: 100px" src="includes/dino_and_earth.jpg">
+</main>
