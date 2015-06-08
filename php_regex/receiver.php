68c9b580b12e4a07a549b5416b676c5dad453530
mkidushim <mkidushim@gmail.com>
2015-06-08 13:00:25 -0700

    done with regex 3.0

--- php_regex/receiver.php
+++ php_regex/receiver.php
@@ -15,8 +15,8 @@ print('<br/>');
 foreach ($_POST as $key => $value) {
 	//print("$key: " .  "$value");
 	print('<br/>');
-	switch ($key) {
-		case 'first_name':
+switch ($key) {
+	case 'first_name':
 		if(preg_match('/^[a-zA-Z]{2,}$/', $_POST['first_name']) != 1){
 			print('first name incorrect');
 			print('<br/>');
@@ -26,16 +26,24 @@ foreach ($_POST as $key => $value) {
 		print("checking first_name : $_POST[first_name]");
 		print('<br/>');
 		break;
-		case 'username':
+	case 'username':
+	if(preg_match('/^[A-Za-z]+[A-Za-z0-9]{8,}$/', $_POST['username']) != 1){
+		print('username cannot be used');
+		break;
+	}
 			print("checking username : $_POST[username]");
 			print('<br/>');
 			break;
-		case 'phone':
+	case 'phone':
+		if(preg_match('/^[0-9]{3}[0-9]{3}[0-9]{4}$/', $_POST['phone']) != 1){
+			print('incorrect phone input');
+			break;
+			}
 			print("checking phone : $_POST[phone]");
 			print('<br/>');
 			break;
-		case 'age':
-		if(preg_match('/^[0-9]{3}$/', $_POST['age']) != 1){
+	case 'age':
+		if(preg_match('/^[0-9]{1,3}$/', $_POST['age']) != 1){
 			print('age incorrect');
 			print('<br/>');
 			break;
@@ -44,11 +52,16 @@ foreach ($_POST as $key => $value) {
 			print('<br/>');
 			break;
 			
-		case 'bonus':
+	case 'bonus':
+		if(preg_match('/^d{3}$/', $_POST['bonus']) != 1){
+			print('BONUS incorrect');
+			print('<br/>');
+			break;
+			}
 			print("checking bonus : $_POST[bonus]");
 			print('<br/>');
 			break;
-		default:
+	default:
 			# code...
 			break;
 	}
