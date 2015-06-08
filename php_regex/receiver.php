5901605c5b7dc75d487a465447d743ef89b12c4d
mkidushim <mkidushim@gmail.com>
2015-06-08 11:48:12 -0700

    regex prototype 1.0 2.0 3.0

--- php_regex/receiver.php
+++ php_regex/receiver.php
@@ -1,2 +1,56 @@
 <?php
+$name = $_POST['first_name'];
+$username = $_POST['username'];
+$phone = $_POST['phone'];
+$age = $_POST['age'];
+
+print($name);
+print('<br/>');
+print($age);
+print('<br/>');
+print($username);
+print('<br/>');
+print($phone);
+print('<br/>');
+foreach ($_POST as $key => $value) {
+	//print("$key: " .  "$value");
+	print('<br/>');
+	switch ($key) {
+		case 'first_name':
+		if(preg_match('/^[a-zA-Z]{2,}$/', $_POST['first_name']) != 1){
+			print('first name incorrect');
+			print('<br/>');
+			break;
+			
+		}
+		print("checking first_name : $_POST[first_name]");
+		print('<br/>');
+		break;
+		case 'username':
+			print("checking username : $_POST[username]");
+			print('<br/>');
+			break;
+		case 'phone':
+			print("checking phone : $_POST[phone]");
+			print('<br/>');
+			break;
+		case 'age':
+		if(preg_match('/^[0-9]{3}$/', $_POST['age']) != 1){
+			print('age incorrect');
+			print('<br/>');
+			break;
+			}
+			print("checking age : $_POST[age]");
+			print('<br/>');
+			break;
+			
+		case 'bonus':
+			print("checking bonus : $_POST[bonus]");
+			print('<br/>');
+			break;
+		default:
+			# code...
+			break;
+	}
+}
 ?>
