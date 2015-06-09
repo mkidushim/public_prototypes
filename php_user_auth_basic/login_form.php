b67246f53dd806534caa8ebb86cdad273a68c1b4
mkidushim <mkidushim@gmail.com>
2015-06-09 12:11:06 -0700

    user authentication 2.0 done

--- php_user_auth_basic/login_form.php
+++ php_user_auth_basic/login_form.php
@@ -1,1 +1,50 @@
-login_form.php
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<title></title>
+	
+</head>
+<body>
+<form>
+<input id="user" type="text" name="username">
+<input id="pass" type="password" name="password">
+<button id="btn" type="button">login</button>
+</form>
+</body>
+<script type="text/javascript">
+function ajax_call() {
+	$.ajax({
+			url: 'login_handler.php',
+			data: {
+				username: $('#user').val(),
+				password: $('#pass').val()
+			},
+			method: "POST",
+			dataType: 'text',
+			success: function(response){
+				window.global_result = response;
+				if(response){
+					console.log('result is true',response)
+				}
+				else if (response == false){
+					console.log(response)
+				}
+			}
+		});
+}
+$(document).ready(function(){
+
+	$('form').on('click','#btn', function(){
+		console.log('btn works')
+		ajax_call();
+		
+	})
+
+})
+	</script>
+</html>
--- php_user_auth_basic/login_handler.php
+++ php_user_auth_basic/login_handler.php
@@ -1,1 +1,26 @@
-login_handler.php
+<?php 
+
+$user_info = [
+["username" => 'shibby', "password" => 'test', 'id'=> 0]
+];
+$user = $_POST['username'];
+$pass = $_POST['password'];
+print_r($user_info);
+foreach ($user_info as $key => $value) {
+	if ($value['username'] == $user) {
+		if ($value['password'] == $pass){
+			$_SESSION['user_id']= $value['id'];
+			print('user info matches');
+		}
+		else{
+			print('user info does not match');
+		session_unset();
+		}
+	}
+	else {
+		print('user info does not match');
+		session_unset();
+	};
+}
+
+?>
