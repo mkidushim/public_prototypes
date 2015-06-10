4da76a9f45e3d945b94686417eab7a2f8e5fb688
mkidushim <mkidushim@gmail.com>
2015-06-10 12:23:07 -0700

    Finished Prototype 3.0

--- php_directory_operations/carousel.php
+++ php_directory_operations/carousel.php
@@ -4,9 +4,9 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<title></title>
 <script type="text/javascript">
-
-img_array = [];
-
+var current_img_index = 0;
+var img_array = [];
+var element_array = [];
 function load_files (){
 $.ajax({
             url: 'dir_listing.php',
@@ -17,6 +17,7 @@ $.ajax({
             	console.log(response.files);
                 var files = response.files;
                 img_array = img_array.concat(response.files);
+
                 var img = $('<img>',{
                         src: response.files[0],
                     })
@@ -37,7 +38,15 @@ $.ajax({
             }
         });
 }
+
 function prev (){
+    if (current_img_index == 0){
+        return
+    }
+ $('img').eq(current_img_index).animate({left: '100%'})
+    $('img').eq(current_img_index-1).animate({left: "0"})
+current_img_index--;
+  /*
    var p = img_array.indexOf($('img').attr('src'));
    console.log('prev img:',p);
  var current_img = p-1;
@@ -49,9 +58,25 @@ function prev (){
    else {
     $('img').attr('src',img_array[p-1])
    }
-
+*/
 }
 function next () {
+    if(current_img_index == 6){
+        return
+    }
+    $('img').eq(current_img_index).animate({left: '100%'})
+    $('img').eq(current_img_index+1).animate({left: "0"})
+    /*
+$('img').eq(current_img_index).removeClass('img_show')
+$('img').eq(current_img_index).addClass('img_hidden')
+$('img').eq(current_img_index+1).removeClass('img_hidden')
+$('img').eq(current_img_index+1).addClass('img_shown')
+
+img_array[current_img_index].animate({
+    left: 100%;
+})*/
+current_img_index++;
+    /*
     var n = img_array.indexOf($('img').attr('src'));
     console.log('prev img:',n);
     var current_img = n+1;
@@ -63,6 +88,7 @@ function next () {
     else{
         $('img').attr('src',img_array[n+1])
     }
+    */
 }
 function intitialize (){
     $.ajax({
@@ -71,19 +97,29 @@ function intitialize (){
             method: 'GET',
             cache: false,
             success: function(response) {
+                //img_array = img_array.concat(response.files);
                 for(var i = 0; i < response.files.length; i++){
+                    img_array.push(response.files[i]);
+                if (i == 0) {
                     var img = $('<img>',{
-                src: response.files[i],
-                class: "img"
-            })
-              $('#img_box').append(img)
+                    src: response.files[i],
+                    class: 'img_shown'
+                    })
+                    $('#img_box').append(img)
                 }
-              
+                else {
+                var img = $('<img>',{
+                src: response.files[i],
+                })
+                $('#img_box').append(img)
+                }
+            
+                }
             }
         })    
 }
 $(document).ready(function(){
-    //intitialize();
+    intitialize();
     $("body").on("click", "#btn", function(){
         load_files();
     })
@@ -99,12 +135,19 @@ $(document).ready(function(){
 #img_box {
     width: 800px;
     height: 600px;
-    position: absolute;
-}
-.img {
-    display: inline-block;
     position: relative;
-    left: 100%;
+    overflow: hidden;
+}
+img {
+position: absolute;
+left: 100%;
+top: 0;
+}
+.img_shown {
+    position: absolute;
+    left:0;
+    top: 0;
+
 }
 </style>
 </head>
