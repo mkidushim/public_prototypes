4c9149fe04e82134376eb30c65aa0fdd5511af60
mkidushim <mkidushim@gmail.com>
2015-06-10 11:01:34 -0700

    finished carousel upto 2

--- php_directory_operations/carousel.php
+++ php_directory_operations/carousel.php
@@ -39,7 +39,9 @@ $.ajax({
 }
 function prev (){
    var p = img_array.indexOf($('img').attr('src'));
-   console.log(p);
+   console.log('prev img:',p);
+ var current_img = p-1;
+ console.log('current_img:', current_img)
    $('img').attr('src','');
    if (p == 0){
     $('img').attr('src',img_array[img_array.length-1])
@@ -51,7 +53,9 @@ function prev (){
 }
 function next () {
     var n = img_array.indexOf($('img').attr('src'));
-    console.log(n);
+    console.log('prev img:',n);
+    var current_img = n+1;
+    console.log('current_img:', current_img)
     $('img').attr('src','');
     if (n == 6){
         $('img').attr('src',img_array[0])
@@ -60,7 +64,26 @@ function next () {
         $('img').attr('src',img_array[n+1])
     }
 }
+function intitialize (){
+    $.ajax({
+            url: 'dir_listing.php',
+            dataType: 'json',
+            method: 'GET',
+            cache: false,
+            success: function(response) {
+                for(var i = 0; i < response.files.length; i++){
+                    var img = $('<img>',{
+                src: response.files[i],
+                class: "img"
+            })
+              $('#img_box').append(img)
+                }
+              
+            }
+        })    
+}
 $(document).ready(function(){
+    //intitialize();
     $("body").on("click", "#btn", function(){
         load_files();
     })
@@ -72,6 +95,18 @@ $(document).ready(function(){
     })
 })
 </script>
+<style type="text/css">
+#img_box {
+    width: 800px;
+    height: 600px;
+    position: absolute;
+}
+.img {
+    display: inline-block;
+    position: relative;
+    left: 100%;
+}
+</style>
 </head>
 <body>
 <button type="button" id="btn">update</button>
