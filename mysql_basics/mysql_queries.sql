f37319b896aa23fc9eb0ee2867428b69225e7527
mkidushim <mkidushim@gmail.com>
2015-06-11 11:06:14 -0700

    finished prototype 1 and 2

--- mysql_basics/mysql_queries.sql
+++ mysql_basics/mysql_queries.sql
@@ -1,1 +1,15 @@
-
+SELECT `username` FROM `user` where `username`='owl'
+UPDATE `user` SET `email`='myawesomeemail' WHERE `username`='owl'
+INSERT INTO `user` SET `username`='Mandy', `email`='greatemail@email.com', `password`=sha1('thisisagreatpassword')
+DELETE FROM `user` WHERE `username`='Mandy'
+INSERT INTO `todo items` SET `title`= 'one', `details`= 'asdf'
+INSERT INTO `todo items` SET `title`= 'two', `details`= 'asdf'
+INSERT INTO `todo items` SET `title`= 'three', `details`= 'asdf'
+INSERT INTO `todo items` SET `title`= 'four', `details`= 'asdf'
+INSERT INTO `todo items` SET `title`= 'five', `details`= 'asdf'
+INSERT INTO `todo items` SET `title`= 'six', `details`= 'asdf'
+INSERT INTO `todo items` SET `title`= 'seven', `details`= 'asdf'
+INSERT INTO `todo items` SET `title`= 'eight', `details`= 'asdf'
+INSERT INTO `todo items` SET `title`= 'nine', `details`= 'asdf'
+INSERT INTO `todo items` SET `title`= 'ten', `details`= 'asdf'
+UPDATE `todo items` SET `title`='mynewtodo',`details`='asdfas' WHERE `ID`='2'
